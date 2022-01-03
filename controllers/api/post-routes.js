8cccc1035df59641c8dbbaba4a1e73e8c11a1e35
Ayla122 <ayladd122@gmail.com>
2022-01-03 14:30:26 -0800

    added update and create post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -15,7 +15,9 @@ router.post("/", withAuth, (req, res) => {
 });
 
 router.put("/:id", withAuth, (req, res) => {
+  console.log(req.body, req.params.id)
   Post.update(req.body, {
+
     where: {
       id: req.params.id
     }
@@ -33,6 +35,7 @@ router.put("/:id", withAuth, (req, res) => {
 });
 
 router.delete("/:id", withAuth, (req, res) => {
+  console.log(req.body, req.params.id)
   Post.destroy({
     where: {
       id: req.params.id
