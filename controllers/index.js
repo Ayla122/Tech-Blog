f0a78a4c60764af49180e2745231a17eeebe2d95
Ayla122 <ayladd122@gmail.com>
2022-01-03 14:09:02 -0800

    setup more modelues

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./api/home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./home-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
